e45a3bafa08c94cfc9cccd2ed3a8a460e2f07796
Colin Lin <colinli@microsoft.com>
2018-02-21 12:08:18 -0800

    update setup ycm

--- setup_ycm.sh
+++ setup_ycm.sh
@@ -2,14 +2,18 @@
 
 
 echo "Prerequisites for installing YCM"
-sudo apt-get install build-essential cmake python-dev python3-dev 
+sudo apt-get install build-essential cmake python-dev python3-dev
 echo "cd to ~/.vim/bundle/YouCompleteMe"
-cd ~/.vim/bundle/YouCompleteMe && ./install.py --clang-completer 
+cd ~/.vim/bundle/YouCompleteMe && ./install.py --clang-completer
 EXTRA_CONFIG_FILE=.ycm_extra_conf.py
 if [[ -e $EXTRA_CONFIG_FILE ]]; then
   echo "Found $EXTRA_CONFIG_FILE!"
 else
   echo "$EXTRA_CONFIG_FILE not found, please provide"
+  exit 1
 fi
 
-echo "YCM setup ready!"
+echo "Copying the .ycm_extra_conf.py file to .vim/bundle/YouCompleteMe/"
+cp $EXTRA_CONFIG_FILE ~/.vim/bundle/YouCompleteMe/ && echo "copy complete~"
+
+echo " == YCM is done setup =="
