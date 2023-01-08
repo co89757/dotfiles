807ff1b51be36f021fad9880a66413362f3819bf
colin <colinlin@pm.me>
2023-01-07 21:51:55 -0800

    Add util for ss-config -> outline key

---
+++ ss2outline.py
@@ -0,0 +1,8 @@
+from base64 import b64encode
+
+METHOD = input('METHOD, e.g. chacha20-ietf-poly1305 -->')
+PASSWORD = input('password-->')
+PORT = input("port--> ")
+IP = input("IP or domain--> ")
+BASE64 = b64encode(f"{METHOD}:{PASSWORD}".encode('ascii'))
+print(f"ss://{BASE64.decode()}@{IP}:{PORT}")
